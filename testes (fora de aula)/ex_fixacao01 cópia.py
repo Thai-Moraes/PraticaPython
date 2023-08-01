26d1da680cb124a5aecd5941683b816a5d972a04
Thai-Moraes <thaissa.linguika@gmail.com>
2023-08-01 11:02:31 -0300

    Primeiro teste no ex_fixacao01 cópia.py

--- testes (fora de aula)/ex_fixacao01 cópia.py
+++ testes (fora de aula)/ex_fixacao01 cópia.py
@@ -1,4 +1,10 @@
-dolar = float(input("Valor em Dólares: $"))
+selecao = int(input("Você deseja usar que tipo de conversão hoje? \n1. Dólar --> Real \n2. Real --> Dólar\n"))
 dolar_hoje = 4.78
 
-print(f"Conversão para Reais (Dólar -> Real): R${dolar * dolar_hoje}")
+if selecao == 1:
+    dolar = float(input("Valor em Dólares: $"))
+    print(f"Conversão para Reais (Dólar -> Real): R${dolar * dolar_hoje}")
+
+if selecao == 2:
+    real = float(input("Valor em Reais: R$"))
+    print(f"Conversão para Dólares (Real -> Dólar): ${real / dolar_hoje}")
