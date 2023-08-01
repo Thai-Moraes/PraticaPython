93f8c891086f486df9661527169bcea79cc50c65
Thai-Moraes <thaissa.linguika@gmail.com>
2023-08-01 11:10:24 -0300

    Novo exercício do SENAI, começo de um joguinho de luta

---
+++ ex_fixacao02.py
@@ -0,0 +1,3 @@
+conta = float(input("Conta a ser paga pelo restaurante: R$"))
+
+print(f"Valor da conta com o acréscimo do garçom: R${conta + (conta * 0.10)}")
---
+++ testes (fora de aula)/jogo-de-luta.py
@@ -0,0 +1,3 @@
+import random
+
+ataques = {1 : ''}
