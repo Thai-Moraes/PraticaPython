c6c5da0a4e866b3d62bc2332d9dfabb3e27bf6d1
Thai-Moraes <thaissa.linguika@gmail.com>
2023-08-01 11:12:02 -0300

    Erro de português :P

--- ex_fixacao02.py
+++ ex_fixacao02.py
@@ -1,3 +1,3 @@
-conta = float(input("Conta a ser paga pelo restaurante: R$"))
+conta = float(input("Conta a ser paga no restaurante: R$"))
 
 print(f"Valor da conta com o acréscimo do garçom: R${conta + (conta * 0.10)}")
