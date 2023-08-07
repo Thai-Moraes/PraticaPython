a8200f634acfab6dc0733d959cfde5520daf5f56
Thai-Moraes <thaissa.linguika@gmail.com>
2023-08-07 11:26:34 -0300

    Acrescentado escolha de personagens, logo e arrumado o sistema de esquiva

--- testes (fora de aula)/jogo-de-luta.py
+++ testes (fora de aula)/jogo-de-luta.py
@@ -1,18 +1,37 @@
 import random
 import time
 import os
-nome = input("Qual o seu nome? ").upper()
 
-ataques = {1 : ['Ataque Fraco', 10, 3], 2 : ['Ataque Forte', 25, 10], 3 : ['Soquinho', 3, 0]}
-ataques_rival = {1 : ['Ataque Congelante', 15, 4], 2 : ['Ataque Supremo', 30, 15], 3 : ['Chute Fraco', 4, 0]}
+print(r"""
+   _____                 __         .__     ____  __.            ___.           __
+  /     \   ____________/  |______  |  |   |    |/ _|____   _____\_ |__ _____ _/  |_
+ /  \ /  \ /  _ \_  __ \   __\__  \ |  |   |      < /  _ \ /     \| __ \\__  \\   __\
+/    Y    (  <_> )  | \/|  |  / __ \|  |__ |    |  (  <_> )  Y Y  \ \_\ \/ __ \|  |
+\____|__  /\____/|__|   |__| (____  /____/ |____|__ \____/|__|_|  /___  (____  /__|
+        \/                        \/               \/           \/    \/     \/
+                        """)
+
+nome = input("Qual o seu nome?: ").upper()
+personagens = {1 : ['Subzero'], 2 : ['Kitana'], 3: ['Scorpion']}
+
+inimigo = int(input(f"\n\nQuem você deseja enfrentar?\n1. {personagens[1][0]} (Fácil)\n2. {personagens[2][0]} (Médio) \n3. {personagens[3][0]} (Difícil)\n"))
+
+if inimigo == 1:
+    ataques_rival = {1: ['Ataque Congelante', 13], 2: ['Chute Gelado', 15], 3: ['Chute Fraco', 3]}
+
+if inimigo == 2:
+    ataques_rival = {1: ['Ataque Assassino', 12], 2: ['Fan-Nado', 20], 3: ['Chute Fraco', 5]}
+
+if inimigo == 3:
+    ataques_rival = {1: ['Soco Alto', 15], 2: ['Corrente', 30], 3: ['Corte Alto', 7]}
+
+ataques = {1 : ['Ataque Fraco', 10], 2 : ['Ataque Forte', 25], 3 : ['Soquinho', 3]}
 
 vida_total = 150
 vida = vida_total
 vida_rival = vida_total
 
-energia_total = 50
-energia = energia_total
-energia_rival = energia_total
+os.system('cls')
 
 def Luta():
 
@@ -20,16 +39,11 @@ def Luta():
     global vida_total
     global vida_rival
 
-    global energia
-    global energia_rival
-    global energia_total
-
     while vida <= 0 or vida_rival <= 0:
         if vida <= 0:
             novamente = input("\nVOCÊ PERDEU! TENTAR NOVAMENTE? (S/N): ")
             if novamente == 's' or novamente == 'S':
                 vida = vida_rival = vida_total
-                energia = energia_rival = energia_total
             else:
                 exit()
 
@@ -37,35 +51,32 @@ def Luta():
             novamente = input("\nVOCÊ GANHOU! TENTAR NOVAMENTE? (S/N): ")
             if novamente == 's' or novamente == 'S':
                 vida = vida_rival = vida_total
-                energia = energia_rival = energia_total
             else:
                 exit()
 
-    print(f'\n{nome}: \nVida: {vida}/{vida_total}\nEnergia: {energia}/{energia_total}\nAtaques: {ataques[1][0]}, {ataques[2][0]}, {ataques[3][0]}')
+    print(f'\n{nome}: \nVida: {vida}/{vida_total}\nAtaques: {ataques[1][0]}, {ataques[2][0]}, {ataques[3][0]}')
 
 
-    print(f'\nSUBZERO: \nVida: {vida_rival}/{vida_total}\nEnergia: {energia_rival}/{energia_total}\nAtaques: {ataques_rival[1][0]}, {ataques_rival[2][0]}, {ataques_rival[3][0]}')
+    print(f'\n{personagens[inimigo][0].upper()}: \nVida: {vida_rival}/{vida_total}\nAtaques: {ataques_rival[1][0]}, {ataques_rival[2][0]}, {ataques_rival[3][0]}')
 
     ataque_esc = int(input(f'\n[SELECIONE SEU ATAQUE!]\n1. {ataques[1][0]}\n2. {ataques[2][0]}\n3. {ataques[3][0]}\n'))
 
-    ataque_esc_rival = random.choice([1,3])
+    ataque_esc_rival = random.choice([1, 2, 3])
 
-    esquiva = random.choice([1,50])
-    esquiva_rival = random.choice([1,50])
+    esquiva = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    esquiva_rival = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
-    if esquiva <= 10:
+    if esquiva <= 2:
         print(f'\n{nome.upper()} DESVIOU DE {ataques_rival[ataque_esc_rival][0].upper()}!')
-    if esquiva_rival <= 10:
-        print(f'\nSUBZERO DESVIOU DE {ataques[ataque_esc][0].upper()}!')
-
-    if esquiva > 10:
+    if esquiva > 2:
         vida = vida - ataques_rival[ataque_esc_rival][1]
-        energia = energia - ataques_rival[ataque_esc_rival][2]
         print(f'\n{nome.upper()} FOI ATINGIDO POR {ataques_rival[ataque_esc_rival][0].upper()}!')
-    if esquiva_rival > 10:
+
+    if esquiva_rival <= 2:
+        print(f'\n{personagens[inimigo][0].upper()} DESVIOU DE {ataques[ataque_esc][0].upper()}!')
+    if esquiva_rival > 2:
         vida_rival = vida_rival - ataques[ataque_esc][1]
-        energia_rival = energia_rival - ataques[ataque_esc][2]
-        print(f'\nSUBZERO FOI ATINGIDO POR {ataques[ataque_esc][0].upper()}!')
+        print(f'\n{personagens[inimigo][0].upper()} FOI ATINGIDO POR {ataques[ataque_esc][0].upper()}!')
 
     time.sleep(3)
 
